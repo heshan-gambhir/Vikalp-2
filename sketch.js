94c8434865624bf96f99f65bdd10d5798bae902d
Heshan Gambhir <64364248+heshan-gambhir@users.noreply.github.com>
2021-01-26 19:44:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var narrator;
+var narrator1;
+var gameState=0;
+var database; 
+var player;
+var playerCount;
+var form;
+var start;
+var start;
+var getState;
+var play;
+
+function preload(){
+
+}
+function setup(){
+    createCanvas(displayWidth-50,displayHeight-150)
+    database = firebase.database();
+    game = new Game();
+    game.getState();
+    game.start();
+}
+function draw(){
+    if(playerCount === 1){
+        game.update(1);
+      }
+      if(gameState === 1){
+        clear();
+        game.play();
+      }
+      if(gameState === 2){
+        game.end();
+      }
+    }
+    
+    
+
